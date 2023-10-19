0e2ec235ba7e99bbb0c9847c28a5836f9d65a073
Ezequiel Sousa Baldez <ezequielbaldez1@gmail.com>
2023-10-18 21:51:38 -0300

    "Entrega" Recuperacção Paralela.

--- RecuperacaoParalela/RecuperacaoParalela/Form1.Designer.cs
+++ RecuperacaoParalela/RecuperacaoParalela/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            button1 = new Button();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            lblPesquisa = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(29, 94);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(29, 65);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(238, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.Control;
+            panel1.Location = new Point(288, 61);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(633, 351);
+            panel1.TabIndex = 6;
+            // 
+            // lblPesquisa
+            // 
+            lblPesquisa.AutoSize = true;
+            lblPesquisa.Location = new Point(29, 47);
+            lblPesquisa.Name = "lblPesquisa";
+            lblPesquisa.Size = new Size(53, 15);
+            lblPesquisa.TabIndex = 7;
+            lblPesquisa.Text = "Pesquisa";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ControlDark;
+            ClientSize = new Size(965, 562);
+            Controls.Add(lblPesquisa);
+            Controls.Add(panel1);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Label lblPesquisa;
     }
 }
